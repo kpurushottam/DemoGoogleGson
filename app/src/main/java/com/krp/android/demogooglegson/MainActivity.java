c2f439302af92ac1370a63658b2a8ebd4e7b79b2
unknown <purushottam.kumar@user492.aepl.com>
2015-11-26 17:52:31 +0530

    Update: add up String Builder appen() method instead of String-Concatination

--- app/src/main/java/com/krp/android/demogooglegson/MainActivity.java
+++ app/src/main/java/com/krp/android/demogooglegson/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -12,12 +13,17 @@ import com.google.gson.Gson;
 import org.json.JSONObject;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvJsonString, tvJsonObj, tvClassDef;
+    private TextView tvJsonString, tvJsonObj;
+    private TextView tvLoadClass, tvClassModifiers, tvClassDef;
 
     private String jsonString = "{ \"username\":\"test\",\"password\":\"123\", \"type\":\"Student\" }";
+
+    private Class testClass;
+    private StringBuilder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         tvJsonString = (TextView) findViewById(R.id.tv_json_string);
         tvJsonObj = (TextView) findViewById(R.id.tv_json_obj);
+        tvLoadClass = (TextView) findViewById(R.id.tv_load_class);
+        tvClassModifiers = (TextView) findViewById(R.id.tv_load_class_modifiers);
         tvClassDef = (TextView) findViewById(R.id.tv_class_def);
 
         tvJsonString.setText(jsonString);
@@ -40,12 +48,40 @@ public class MainActivity extends AppCompatActivity {
                 "user-type: "+userObj.getType());
     }
 
+    public void onLoadClass(View v) {
+        builder = new StringBuilder();
+        // testClass = TestClassReflection.class;
+        // or
+        try {
+            testClass = Class.forName("com.krp.android.demogooglegson.TestClassReflection");
+            builder.append("Loaded successfully\n");
+            builder.append("Class Name: \n").append(testClass.getSimpleName()).append("\n");
+            builder.append("Class Name: \n").append(testClass.getName());
+            tvLoadClass.setText(builder.toString());
+            tvLoadClass.setTextColor(getResources().getColor(android.R.color.black));
+        } catch (ClassNotFoundException e) {
+            tvLoadClass.setText(e.getClass().getSimpleName() + "\nexception occured");
+            tvLoadClass.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+        }
+    }
+
+    public void onLoadClassModifiers(View v) {
+        if(testClass == null) {
+            tvLoadClass.setText("Load class first");
+            tvLoadClass.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+            return;
+        }
+
+        int modifiers = testClass.getModifiers();
+        tvClassModifiers.setText(Modifier.toString(modifiers));
+    }
+
     public void onReflectClassUser(View v) {
         Method[] methods = TestClassReflection.class.getMethods();
-        StringBuilder builder = new StringBuilder();
+        builder = new StringBuilder();
 
         for(Method method : methods) {
-            builder.append(method.getName()+"\n");
+            builder.append(method.getName()).append("\n");
         }
 
         tvClassDef.setText(builder.toString());
